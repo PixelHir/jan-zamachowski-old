7290927cd605ef40f2bc7dfadb3e4178f4c18bbe
Jędrzej <jedras@jedrasgame.pl>
2017-01-29 19:18:22 +0100

    testowe wykrywanie przeklenstw fix

--- bot.js
+++ bot.js
@@ -5,6 +5,10 @@ cmd1 = /^\/color/,
 cmd2 = /^\/echo/,
 cmd3 = /^\/emoji/,
 cmd4 = /^\/search/,
+swear1 = /kurwa/i,
+swear2 = /huj/i,
+swear3 = /pierdole/i,
+swear4 = /pierdolę/i,
 login( {
     email: process.env.FB_USERNAME,
     password: process.env.FB_PASSWORD
@@ -211,7 +215,17 @@ login( {
                         };
 
                         api.sendMessage(msg, event.threadID);
+                    } else if(event.body[0] === "/") //MUSI BYC NA KONCU
+                    {
+                        var msg = {
+                            body: "\"" + event.body + "\": Nie odnaleziono podanego polecenia"
+                        };
+
+                        api.sendMessage(msg, event.threadID);
+                    } else if(swear1.test(event.body)||swear2.test(event.body)||swear3.test(event.body)||swear4.test(event.body)) {
+                        api.sendMessage("Nie klnij tyle śmieciu", event.threadID);
                     }
+  
 
 
 
@@ -222,24 +236,11 @@ login( {
 
 
                     //Licznik przekleństw
-                    swear1 = /kurwa/i,
-                    swear2 = /huj/i,
-                    swear3 = /pierdole/i,
-                    swear4 = /pierdolę/i,
-                    if(swear1.test(event.body)||swear2.test(event.body)||swear3.test(event.body)||swear4.test(event.body)) {
-                        api.sendMessage("Nie klnij tyle śmieciu", event.threadID);
-                    }
+                    
+                    
 
-
-                    //MUSI BYC NA KONCU
-                    else if(event.body[0] === "/")
-                    {
-                        var msg = {
-                            body: "\"" + event.body + "\": Nie odnaleziono podanego polecenia"
-                        };
-
-                        api.sendMessage(msg, event.threadID);
-                    }   
+                    
+                    
                 }
                 
                 api.markAsRead(event.threadID, function(err) {
