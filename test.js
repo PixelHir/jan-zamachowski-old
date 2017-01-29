64d1b74ffe2e6233399201922d6ad34da2691f38
Jędrzej <jedras@jedrasgame.pl>
2017-01-29 15:48:57 +0100

    kod testowy dla travisa

---
+++ test.js
@@ -0,0 +1,12 @@
+var login = require("facebook-chat-api");
+//Skrypt testowy dla travisa
+
+login( {
+    email: process.env.FB_USERNAME,
+    password: process.env.FB_PASSWORD
+}, function callback (err, api) {
+    if(err)
+        return console.error(err);
+        }
+    });
+});
