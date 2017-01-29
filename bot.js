d46dee53349e41342589b255270aa3dfff027d81
Jędrzej <jedras@jedrasgame.pl>
2017-01-29 20:32:03 +0100

    baza danych pt.2

--- bot.js
+++ bot.js
@@ -243,15 +243,16 @@ login( {
                         var swodzywki = ["Nie klnij tyle śmieciu", "Mama wie jak przeklinasz?", "JAKI DAJESZ KURWA PRZYKŁAD ŚMIECIU PIERDOLONY"]
                         var selswodzywki = swodzywki[Math.floor(Math.random() * swodzywki.length)];
                         connection.query("INSERT INTO `swears` (`USERID`, `COUNT`) VALUES (" + event.senderID +", 1) ON DUPLICATE KEY UPDATE count=count+1;");
-                        var query=connection.query("SELECT * FROM `swears` WHERE `USERID` = 110001862348398");
-                        var swearuserc = 0;
-                        query.on('result', function(row) {
-                            swearuserc = row.COUNT;
-                        });
-                        query.on('error'), function(row) {
-                            console.log(error);
-                        }
-                        api.sendMessage(selswodzywki + " To już " + swearuserc + " raz...", event.threadID);
+                        //var query=connection.query("SELECT * FROM `swears` WHERE `USERID` = 110001862348398");
+                        //var swearuserc = 0;
+                        //query.on('result', function(row) {
+                        //    swearuserc = row.COUNT;
+                        //});
+                        //query.on('error'), function(row) {
+                        //   console.log(error);
+                        //}
+                        //api.sendMessage(selswodzywki + " To już " + swearuserc + " raz...", event.threadID);
+                        api.sendMessage(selswodzywki, event.threadID);
                         
                         
                     }
