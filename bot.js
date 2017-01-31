4bd81e3dbd7292dadfd76ec62f431aa85009baa7
adampisula <adam.pisula@outlook.com>
2017-01-31 08:42:22 +0100

    Poprawka

--- bot.js
+++ bot.js
@@ -227,11 +227,15 @@ var commands = [
         syntax: "",
         desc: "Zwraca zawartość tabeli 'info'",
         func: (api, event, args) => {
-            api.sendMessage(JSON.stringify(connection.query("SELECT * FROM info"), null, 4), event.threadID);
+        			var obj = connection.query("SELECT * FROM info");
+        			
+            api.sendMessage(obj, censor(obj), 4), event.threadID);
         },
         hidden: true
     },
 ];
+
+function censor(censor) { var i = 0; return function(key, value) { if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) return '[Circular]'; if(i >= 29) return '[Unknown]'; ++i; return value; } }
 
 var connection = mysql.createConnection({
 	host: process.env.DB_HOST,
