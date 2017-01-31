2fea249761ab0b55a6c876ed824f7a72db203a85
Jędrzej <jedras@jedrasgame.pl>
2017-01-31 18:59:00 +0100

    Nick

--- bot.js
+++ bot.js
@@ -269,8 +269,24 @@ var commands = [
             //var text = JSON.stringify(obj, function(key, value) { if (typeof value === 'object' && value !== null) { if (cache.indexOf(value) !== -1) { return; } cache.push(value); } return value; }); cache = null;
             
             //api.sendMessage(text, event.threadID);
-    			}
-	}
+    	}
+	},
+    {
+        cmd: "nick",
+        syntax: " [nazwa]|[nick]"
+        desc: " Zmienia nick użytkownika",
+        func: (api, event, args) => {
+            nickargs = args.split("|", 2);
+            api.getUserID(nickargs[0], function(err, data) {
+                if(err)
+                    return callback(err);
+                idtochange = data[0].userID;
+                api.changeNickname(nickargs[1], event.threadID, idtochange, function callback(err) {
+                    if(err) return console.error(err);
+                });
+            });
+        }
+    }
 ];
 
 var connection = mysql.createConnection({
