594d3def949d7fb9ace8aab2f868f7bfa02e1c6b
Jędrzej <jedras@jedrasgame.pl>
2017-01-31 10:17:57 +0100

    search

--- bot.js
+++ bot.js
@@ -221,6 +221,22 @@ var commands = [
         },
         hidden: true
     },
+	//user search
+	{
+        cmd: "search",
+        syntax: "",
+        desc: "Wyszukuje ID usera",
+        func: (api, event, args) => {
+            api.getUserID(args, function(err, data) {
+        	if(err) return callback(err);
+
+        // Send the message to the best match (best by Facebook's criteria)
+        	var foundID = data[0].userID;
+        	api.sendMessage("Wynik wyszukiwania: " + foundID, threadID);
+   		 });
+        },
+        hidden: false
+    },
     //INFOTBL
     /* {
         cmd: "infotbl",
