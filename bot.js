b7b9fdccebf3393abea3cd132d47ee5ed1d68074
Jędrzej <jedras@jedrasgame.pl>
2017-03-05 13:53:10 +0100

    debug

--- bot.js
+++ bot.js
@@ -352,7 +352,7 @@ var commands = [
                 try {
                 client.get("https://myanimelist.net/api/anime/search.xml?q=" + malargs, function (data, response) {
                 // parsed response body as js object
-                console.log(data);
+                console.log(data.toString());
 
                 var queryanimes = [];
                 data.anime.entry.forEach(function (elem) {
