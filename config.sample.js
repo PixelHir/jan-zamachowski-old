8cf37eaa7bc86b0ff2f4bb2461c1dd3ef3b4dbf0
pixelhir <jedras@jedrasgame.pl>
2019-03-08 22:46:05 +0100

    sample config update

--- config.sample.js
+++ config.sample.js
@@ -3,6 +3,7 @@ module.exports = {
   FB_USERNAME: "user",
   FB_PASSWORD: "pass",
   CLEVERBOT_API: "key",
-  MAL_USERNAME: "user",
-  MAL_PASSWORD: "pass"
+  DB_USERNAME: "user",
+  DB_PASSWORD: "pass",
+  DB_HOST: "host"
 }
