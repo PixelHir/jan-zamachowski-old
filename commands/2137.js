95283888ac17ce697178eb5b6f8d11f31e930d3e
pixelhir <jedras@jedrasgame.pl>
2019-03-11 18:44:14 +0100

    2137 unique random

--- commands/2137.js
+++ commands/2137.js
@@ -5,13 +5,32 @@ module.exports = {
     desc: "INSERT Kremówka;",
     func: (api, event, args) => {
         fs.readdir("./inba", (err, cenzo) => {
-            var chosencenzo = cenzo[Math.floor(Math.random() * cenzo.length)];
+            //var chosencenzo = cenzo[Math.floor(Math.random() * cenzo.length)];
+            uniquerand(cenzo.length, (num) => {
             var msg = {
                 body: "Inba trwa",
-                attachment: fs.createReadStream("./inba/" + chosencenzo)
+                attachment: fs.createReadStream("./inba/" + cenzo[num])
             };
             api.sendMessage(msg, event.threadID);
+            });
         });
     },
     hidden: true
+}
+
+
+var usednums = [];
+function uniquerand(max, callback) {
+	if (usednums.length == max) {
+		usednums = [];
+	}
+
+	var num;
+	num = Math.floor(Math.random() * max);
+	while (usednums.includes(num)) {
+        num = Math.floor(Math.random() * max);
+        console.log('reroll');
+	}
+	usednums.push(num);
+	callback(num);
 }
